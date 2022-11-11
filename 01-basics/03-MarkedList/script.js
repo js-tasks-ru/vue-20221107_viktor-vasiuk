5f380ee2965e0d82f076027786c5ee26269cf13c
Your Name <you@example.com>
2022-11-11 11:03:02 +0300

    init tasks 01-basics

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,30 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      emails,
+      search: '',
+    };
+  },
+
+  computed: {
+    filteredEmails () {
+      const searchFilter = (email) =>
+        email
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+
+      return this.emails.filter((email) => this.search.length > 0 && searchFilter(email))
+    },
+  },
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+//window.vm = vm;
+
