5467fe73d712abb6bc31929e72f47d75b71a4131
Your Name <you@example.com>
2022-12-05 09:00:48 +0300

    master

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -14,54 +14,54 @@
 
     components: { UiInput },
 
-    props: {
-      modelValue: Number,
+  props: {
+    modelValue: Number,
 
       type: {
         type: String,
         default: 'date',
+      validator: (type) => ['date', 'datetime-local', 'time'].includes(type),
         validator: (type) => ['date', 'datetime-local', 'time'].includes(type),
       },
     },
 
     emits: ['update:modelValue'],
 
-    computed: {
-      value() {
-        // No value - empty string
-        if (typeof this.modelValue === 'undefined' || this.modelValue === null) {
-          return '';
-        }
-
-        // YYYY-MM-DDTHH:MM:SS.mssZ
-        const date = new Date(this.modelValue).toISOString();
-
-        if (this.type === 'date') {
-          // https://developer.mozilla.org/ru/docs/Web/HTML/Element/Input/date
-          return date.substring(0, 10); // YYYY-MM-DD
-        } else if (this.type === 'datetime-local') {
-          // https://developer.mozilla.org/ru/docs/Web/HTML/Element/Input/datetime-local
-          return date.substring(0, 16); // YYYY-MM-DDTHH:MM
-        } else if (this.type === 'time') {
-          // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/time
-          return this.$attrs['step'] && this.$attrs['step'] % 60 !== 0
-            ? date.substring(11, 19) // HH:MM:SS
-            : date.substring(11, 16); // HH:MM
-        }
-
-        // Something wrong - empty string as no value
+  computed: {
+    value() {
+      // No value - empty string
+      if (typeof this.modelValue === 'undefined' || this.modelValue === null) {
         return '';
-      },
+      }
+
+      // YYYY-MM-DDTHH:MM:SS.mssZ
+      const date = new Date(this.modelValue).toISOString();
+
+      if (this.type === 'date') {
+        // https://developer.mozilla.org/ru/docs/Web/HTML/Element/Input/date
+        return date.substring(0, 10); // YYYY-MM-DD
+      } else if (this.type === 'datetime-local') {
+        // https://developer.mozilla.org/ru/docs/Web/HTML/Element/Input/datetime-local
+        return date.substring(0, 16); // YYYY-MM-DDTHH:MM
+      } else if (this.type === 'time') {
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/time
+        return this.$attrs['step'] && this.$attrs['step'] % 60 !== 0
+          ? date.substring(11, 19) // HH:MM:SS
+          : date.substring(11, 16); // HH:MM
+      }
+
+      // Something wrong - empty string as no value
+      return '';
     },
 
-    methods: {
-      handleInput($event) {
-        if ($event.target.value === '') {
-          this.$emit('update:modelValue', null);
-          return;
-        }
+  methods: {
+    handleInput($event) {
+      if ($event.target.value === '') {
+        this.$emit('update:modelValue', null);
+        return;
+      }
 
-        this.$emit('update:modelValue', $event.target.valueAsNumber);
+        this.$emit('update:modelValue', $$event.target.valueAsNumber);
       },
     },
   };
