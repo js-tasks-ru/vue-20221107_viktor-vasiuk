09281cc462e7312662713b0e6d4c48ab9972cbb4
Your Name <you@example.com>
2022-12-06 08:27:02 +0300

    task 07-forms/01-CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,37 @@
 <template>
-  <button></button>
+  <button style="width: 50px" @click="click">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: this.count,
+    };
+  },
+
+  watch: {
+    count() {
+      this.localCount = this.count;
+    },
+  },
+
+  methods: {
+    click() {
+      this.localCount++;
+      this.$emit('update:count', this.localCount);
+    },
+  },
 };
 </script>
